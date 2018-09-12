ff9038f4f958a78c00fcfb07dba648d265cde5be
coding-git <38887449+coding-git@users.noreply.github.com>
2018-09-12 14:20:36 -0400

    Fixed rubocop offenses

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -8,67 +8,33 @@ SUBTRACT = "-"
 MULTIPLY = "*"
 DIVIDE = "/"
 
-NAME = 0
-NUMBER = 1
-OPERATOR = 2
-
-def calculator(name)
-  prompt ("Enter the first number:")
-  num1 = validateEntry(gets.chomp, NUMBER)
-
-  prompt("Enter the second number:")
-  num2 = validateEntry(gets.chomp, NUMBER)
-
-  operator_prompt = <<-MSG
-    What operation would you like to perform? Please enter:
-      + for addition
-      - for subtraction
-      * for multiplication
-      / for division
-  MSG
-
-  prompt(operator_prompt)
-  operator = validateEntry(gets.chomp, OPERATOR)
-
-  result = calculate(num1.to_f, num2.to_f, operator)
-  prompt("(#{num1} #{operator} #{num2}) = #{result}")
-
-  prompt("Do you want to do another calculation?")
-  if gets.downcase.start_with?('y')
-    calculator(name)
-  else
-    prompt("Thank you for playing, #{name}.")
-  end
-end
-
 def prompt(message)
   puts("=> #{message}")
 end
 
-def checkNum(num)
-  num == "0" || (num.to_f != 0 && num != "0") ? true : false
+def validate_name(name)
+  while name.empty?()
+    prompt "Please provide a valid name:"
+    return gets.chomp
+  end
+  name
 end
 
-def validateEntry(entry, type)
-  case type
-  when NAME
-    while entry.empty?()
-      prompt("Please provide a valid name:")
-      entry = gets.chomp
-    end
-  when NUMBER
-    until checkNum(entry)
-      prompt("Invalid number. Please enter a numeric value:")
-      entry = gets.chomp
-    end
-  when OPERATOR
-    # until %w(+ - * /).include?(entry)
-    until entry == ADD || SUBTRACT || MULTIPLY || DIVIDE do
-      prompt("Invalid operator. Please enter +, -, *, or /:")
-      entry = gets.chomp
-    end
+def validate_num(num)
+  unless num == "0" || (num.to_f != 0 && num != "0")
+    prompt "Please enter a valid number."
+    num = gets.chomp
   end
-  entry
+  num
+end
+
+def validate_operator(operator)
+  # until %w(+ - * /).include?(entry)
+  until operator == ADD || SUBTRACT || MULTIPLY || DIVIDE
+    prompt "Invalid operator. Please enter +, -, *, or /:"
+    operator = gets.chomp
+  end
+  operator
 end
 
 def calculate(num1, num2, operator)
@@ -84,9 +50,34 @@ def calculate(num1, num2, operator)
   end
 end
 
-prompt("Welcome to the calculator. Enter your name:")
-name = validateEntry(gets.chomp, NAME)
+prompt "Welcome to the calculator. Enter your name:"
+name = validate_name(gets.chomp)
 prompt("Hello #{name}!")
-calculator(name)
 
+loop do
+  prompt "Enter the first number:"
+  num1 = validate_num(gets.chomp)
 
+  prompt "Enter the second number:"
+  num2 = validate_num(gets.chomp)
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform? Please enter:
+      + for addition
+      - for subtraction
+      * for multiplication
+      / for division
+  MSG
+
+  prompt(operator_prompt)
+  operator = validate_operator(gets.chomp)
+
+  result = calculate(num1.to_f, num2.to_f, operator)
+  prompt("(#{num1} #{operator} #{num2}) = #{result}")
+
+  prompt "Do you want to do another calculation?"
+  unless gets.downcase.start_with?('y')
+    prompt("Thank you for playing, #{name}.")
+    break
+  end
+end
